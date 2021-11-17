a95f589fba32c56e326c39080e30dea6d6091139
Gisele Caetano Pereira <87086783+gicaetano@users.noreply.github.com>
2021-11-16 21:39:40 -0300

    :package: first routes

--- index.js
+++ index.js
@@ -2,17 +2,39 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+
 app.get("/", function(req, res) {
-    res.send("Minha primeira requisição");
+    res.send("Minha PRIMEIRA requisição");
 });
 
-app.get("/segunda-req", function(req, res) {
-    res.send("Minha segunda requisição");
+app.get("/segunda", function(req, res) {
+    res.send("Minha SEGUNDA requisição");
 });
+
 
 app.get("/com-parametros", function(req, res) {
-    res.send("Com parametros funfa!!! Sabadou " + req.query.nome);
+    if (req.query.nome === "Gisele"){
+        res.send("Gisele chamou requisição");
+    }
+    res.send("Com parametros funfa!!! Sabadou " + req.query.nome + req.query.sobrenome);
 });
+
+app.post("/meu-primeiro-post", function(req, res) {
+    console.log(req.body)
+    res.send("Meu post funciona");
+});
+
+app.put("/meu-primeiro-put/:id", function(req, res) {
+    console.log(req.body, req.params.id)
+    res.send("Meu put funciona");
+});
+
+app.delete("/meu-primeiro-delete/:id", function (req, res) {
+    console.log(req.params.id);
+    res.send("Meu delete funciona" + req.params.id);
+})
+
 app.listen(port, function() {
-    console.log("Retorno: ", port);
+    console.log("Ouvindo a porta: ", port);
 });
